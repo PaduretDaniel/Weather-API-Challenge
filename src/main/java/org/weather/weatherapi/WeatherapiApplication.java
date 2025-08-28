ce4a4c77f805216ac131853a71499212754ce021
Daniel <dpaduret70@gmail.com>
2025-08-28 20:02:34 +0300

    more configuration

--- src/main/java/org/weather/weatherapi/WeatherapiApplication.java
+++ src/main/java/org/weather/weatherapi/WeatherapiApplication.java
@@ -3,11 +3,14 @@ package org.weather.weatherapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class WeatherapiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WeatherapiApplication.class, args);
+        SpringApplication app = new SpringApplication(WeatherapiApplication.class);
+        System.setProperty("spring.main.web-application-type", "none");
+        app.run(args);
     }
 
 }
